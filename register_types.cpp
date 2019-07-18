bfda6e3021adb4580da9b825ce7e935384d3e353
Jairo Honorio <registrofx@gmail.com>
2019-07-18 18:02:03 -0300

    Fix config.py and register_types codes and move AppsFlyer framework to lib folder

--- register_types.cpp
+++ register_types.cpp
@@ -1,12 +1,14 @@
 #include "register_types.h"
-#if defined(__APPLE__)
+#include "core/class_db.h"
+#include "core/engine.h"
+#ifdef IPHONE_ENABLED
 #include "ios/GodotAppsFlyer.h"
 #endif
 
 void register_godot_appsflyer_types() {
-#if defined(__APPLE__)
-	ClassDB::register_class<GodotAppsFlyer>();
-#endif
+	#ifdef IPHONE_ENABLED
+	Engine::get_singleton()->add_singleton(Engine::Singleton("GodotAppsFlyer", memnew(GodotAppsFlyer)));
+	#endif
 }
 
 void unregister_godot_appsflyer_types() {
