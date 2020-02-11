25906ac4807654065326c6eb2349cf3f46782c8b
mavsm <mathias.vsm@gmail.com>
2020-02-11 15:57:38 -0200

    Add .conf files

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ def configure(env):
         env.Append(FRAMEWORKPATH=['ios/lib'])
         env.Append(LINKFLAGS=['-ObjC', '-framework','SurveyMonkeyLib'])
     elif env['platform'] == 'android':
+        return
         env.android_add_dependency("implementation 'com.surveymonkey:surveymonkey-android-sdk:2.0.0'")
         env.android_appattributes_chunk += ' tools:replace="android:allowBackup" '
         env.android_add_default_config("minSdkVersion 21")
